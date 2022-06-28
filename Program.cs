d9ed64c88e40ca15f6ba92da8c4f89a706ae3182
Nick Rudakov <porol2122@gmail.com>
2022-06-29 00:39:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuniorN8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeOneGuest = 10;
+            Console.WriteLine("Введите количество человек в очереди");
+            int howMuchGuest = int.Parse(Console.ReadLine());
+            int minuts = (timeOneGuest * howMuchGuest) % 60;
+            int hourse = (timeOneGuest * howMuchGuest) / 60;
+            Console.WriteLine("Осталось ждать - " + hourse + " часа " + minuts + " минут");
+            Console.ReadLine();
+        }
+    }
+}
